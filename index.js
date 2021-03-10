a8b9753635e35303893e9456a136f2ef41b23e55
Coyls <76538870+Coyls@users.noreply.github.com>
2021-03-10 16:28:30 +0100

    add : list of user in the same chat room

--- index.js
+++ index.js
@@ -5,26 +5,34 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
 app.use(express.static('public'))
 
-let users = [];
+let users = []
+let rooms = ["generalChat", "test"]
 
 io.on('connection', (socket) => {
     console.log(socket.id)
-    socket.emit('userId', socket.id, "general Chat")
-    socket.join('general Chat');
+    socket.emit('userId', socket.id, rooms)
+    socket.join(rooms[0]);
 
     // console.log(socket)
 
     socket.on('user', login => {
         users.push(login)
-        console.log(login)
-        io.to(login.room).emit('participants' , users)
+
+        let userInSameRoom = []
+
+        users.forEach(user => {
+            if (user.room === login.room) {
+                userInSameRoom.push(user)
+            }
+        })
+
+        io.to(login.room).emit('participants', userInSameRoom)
         io.to(login.room).emit('connect message', `${login.name} vient de se connecter dans ${login.room}`)
     })
 
@@ -37,20 +45,40 @@ io.on('connection', (socket) => {
     })
 
 
-    socket.on('changeRoom', (roomId,login) => {
-            socket.join(roomId);
-            if (login.room !== -1 ) {
-                socket.leave(login.room)
-            }
+    socket.on('changeRoom', (roomId, login) => {
+        socket.join(roomId);
+        socket.leave(login.room)
 
+        const index = users.findIndex(user => user.userId === login.userId)
+        users[index].room = roomId
+        console.log(users)
+
+        // -- A refactor car dupliqur -- //
+        let userInSameRoom = []
+        let userInLastRoom = []
+
+        users.forEach(user => {
+            if (user.room === roomId) {
+                userInSameRoom.push(user)
+            }
+            if (user.room === login.room) {
+                userInLastRoom.push(user)
+            }
         })
+
+        io.to(login.room).emit('participants', userInLastRoom)
+        io.to(login.room).emit('connect message', `${login.name} vient de se deconnecter de ${login.room}`)
+        io.to(roomId).emit('participants', userInSameRoom)
+        io.to(roomId).emit('connect message', `${login.name} vient de se connecter dans ${roomId}`)
+        // ---------------------------------- //
+    })
 
 
     socket.on('disconnect', () => {
         const index = users.findIndex(user => user.userId === socket.id)
 
         if (index !== -1) {
-            io.to(users[index].room).emit('participants' , users)
+            io.to(users[index].room).emit('participants', users)
             io.to(users[index].room).emit('connect message', ` ${users[index].name} vient de se déconnecter de ${users[index].room}`)
             users.splice(index, 1)
         }
